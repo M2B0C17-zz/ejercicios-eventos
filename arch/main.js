671c36005c01f4a9d9cbd89a772793d94cae1b33
macarena baltra <macarena.bc2011@gmail.com>
2017-05-16 19:57:25 -0400

    Subiendo primera parte del trabajo

--- arch/main.js
+++ arch/main.js
@@ -1,1 +1,12 @@
-alert("Hola Mundo");
+var boton = document.getElementById('demo');
+boton.addEventListener('click', function(){
+  alert("!Hola Mundo!");
+  alert("!AJA!!! No era todito xD!");
+  alert("¡Te tengo un REGALITO!");
+  alert("Pon ATENCIÓN <3");
+  alert("1.- Me he comprado unas NIKE en la tienda de deportes en el centro.");
+  alert("2.- ¿Air Max?");
+  alert("3.- Air muchax max.");
+  alert("JAJAJJA fin ....");
+  alert("muajajjaja por el poder el SQUAD 666!!");
+});
